b45b05be6b26cc8be797bb1e015fbcc83b0fb920
WieFel <felix.wielander@gmail.com>
2016-04-28 19:43:05 +0200

    Searching for possible friends now possible
    via URL /usuarios/{usuario}/posiblesAmigos

--- UPMsocial/src/resources/UsuarioResource.java
+++ UPMsocial/src/resources/UsuarioResource.java
@@ -1,5 +1,8 @@
 package resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -7,6 +10,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Request;
@@ -16,6 +20,7 @@ import javax.xml.bind.JAXBElement;
 
 import dao.UsuarioDao;
 import model.Usuario;
+import model.UsuarioList;
 
 @Path("/usuarios")
 public class UsuarioResource {
@@ -63,6 +68,32 @@ public class UsuarioResource {
 			res = Response.noContent().build();
 		}
 		return res;
+	}
+
+	@Path("{usuario}/posiblesAmigos")
+	@GET
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	public Response getPosiblesAmigos(@PathParam("usuario") String id, @QueryParam("name") String name) {
+		int id_int = Integer.parseInt(id);
+		if (UsuarioDao.getInstance().containsId(id_int)) {
+			// User exists
+			List<Usuario> posiblesAmigos = new ArrayList<Usuario>();
+			Usuario u = UsuarioDao.getInstance().getUser(id_int);
+
+			for (Usuario usuario : UsuarioDao.getInstance().getUsers()) {
+				// test if usuario is not the user itself (whose friends we are
+				// looking for), if it is not already
+				// a friend of the user and if it contains the desired name (if
+				// parameter is set)
+				if (usuario.getId() != u.getId() && !u.getAmigos().containsKey(usuario.getId())
+						&& (name == null || usuario.getNombre().toLowerCase().contains(name.toLowerCase())))
+					posiblesAmigos.add(usuario);
+			}
+
+			return Response.ok(new UsuarioList(posiblesAmigos)).build();
+		} else
+			// User doesn't exist
+			return Response.status(Response.Status.NOT_FOUND).build();
 	}
 
 	/**
