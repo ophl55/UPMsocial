ea24a1eb04df536613eefa09050e8259d1c63bcd
WieFel <felix.wielander@gmail.com>
2016-04-29 21:30:40 +0200

    Implementation db in UsuarioDao

--- UPMsocial/src/dao/UsuarioDao.java
+++ UPMsocial/src/dao/UsuarioDao.java
@@ -1,25 +1,28 @@
 package dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 import java.util.NoSuchElementException;
+
+import com.mysql.jdbc.Connection;
 
 import model.Usuario;
 
 public class UsuarioDao {
 
-	private Map<Integer, Usuario> contentProvider = new HashMap<>();
-	private int lastId = 0;
-
 	private static UsuarioDao instance = null;
 
 	private UsuarioDao() {
 
-		Usuario usuario = new Usuario("Usuario1");
-		addUser(usuario);
-		usuario = new Usuario("Usuario2");
-		addUser(usuario);
+		// TODO default inserts
+		// Usuario usuario = new Usuario("Usuario1");
+		// addUser(usuario);
+		// usuario = new Usuario("Usuario2");
+		// addUser(usuario);
 
 	}
 
@@ -30,18 +33,41 @@ public class UsuarioDao {
 	}
 
 	/**
-	 * Adds the user to the contentProvider and sets the generated ID of the
-	 * user. The generated ID is also returned.
+	 * Adds the user to the database. The generated ID is returned.
 	 * 
 	 * @param usuario
 	 *            User to be added (id of user not important)
-	 * @return The generated ID for the user as a String
+	 * @return The generated ID for the user as an int
 	 */
 	public int addUser(Usuario usuario) {
-		usuario.setId(lastId);
-		contentProvider.put(usuario.getId(), usuario);
-		lastId++;
-		return usuario.getId();
+		int id = 0;
+
+		String sql = "INSERT INTO usuario(id,name) VALUES(0,'" + usuario.getNombre() + "');";
+
+		Statement statement = null;
+		Connection connection = DBConnection.getConnection();
+
+		try {
+			System.out.println("Connecting to a selected database...");
+			statement = connection.createStatement();
+			System.out.println("Connected database successfully...");
+
+			statement.executeUpdate(sql);
+			System.out.println("Inserted records into the table...");
+
+			sql = "SELECT @@IDENTITY as 'id'";
+			ResultSet rs = statement.executeQuery(sql);
+			rs.next();
+			id = rs.getInt("id");
+
+		} catch (SQLException se) {
+			// Handle errors for JDBC
+			se.printStackTrace();
+		} finally {
+			// finally block used to close resources
+			DBConnection.closeConnection(statement, connection);
+		} // end try
+		return id;
 	}
 
 	/**
@@ -63,36 +89,98 @@ public class UsuarioDao {
 	 * @return {@code true} if the user is removed successfully
 	 */
 	public boolean removeUserById(int id) {
-		if (!containsId(id))
-			return false;
+		String sql = "DELETE FROM usuario WHERE id = " + "'" + String.valueOf(id) + "';";
 
-		contentProvider.remove(id);
+		Statement statement = null;
+		Connection connection = DBConnection.getConnection();
+		try {
+			System.out.println("Connecting to a selected database...");
+			statement = connection.createStatement();
+			System.out.println("Connected database successfully...");
+
+			statement.executeUpdate(sql);
+			System.out.println("Deleted record from the table...");
+		} catch (SQLException se) {
+			// Handle errors for JDBC
+			se.printStackTrace();
+			return false;
+		} finally {
+			// finally block used to close resources
+			DBConnection.closeConnection(statement, connection);
+		} // end try
 		return true;
 	}
 
 	/**
-	 * Tests if the list contains the user.
+	 * Tests if the db contains the user.
 	 * 
 	 * @param usuario
 	 *            the user to search for.
-	 * @return {@code true} if the user is found in the list
+	 * @return {@code true} if the user is found in the db
 	 */
 	public boolean contains(Usuario usuario) {
-		return contentProvider.containsValue(usuario);
+		return containsId(usuario.getId());
 	}
 
 	/**
 	 * 
 	 * @param id
 	 *            the id to be searched
-	 * @return {@code true} if the id is inside the range of the list
+	 * @return {@code true} if the id is inside the range of the db
 	 */
 	public boolean containsId(int id) {
-		return contentProvider.containsKey(id);
+		String sql = "SELECT count(*) FROM usuario WHERE id = " + "'" + String.valueOf(id) + "';";
+
+		Statement statement = null;
+		Connection connection = DBConnection.getConnection();
+		try {
+			System.out.println("Connecting to a selected database...");
+			statement = connection.createStatement();
+			System.out.println("Connected database successfully...");
+
+			ResultSet rs = statement.executeQuery(sql);
+			rs.next();
+			if (rs.getInt("count(*)") > 0) {
+				System.out.println("Table contains id!");
+				return true;
+			} else
+				System.out.println("Table doesn't contain id!");
+			return false;
+		} catch (SQLException se) {
+			// Handle errors for JDBC
+			se.printStackTrace();
+		} finally {
+			// finally block used to close resources
+			DBConnection.closeConnection(statement, connection);
+		} // end try
+		return false;
 	}
 
 	public Collection<Usuario> getUsers() {
-		return contentProvider.values();
+		List<Usuario> l = new ArrayList<Usuario>();
+
+		String sql = "SELECT * FROM usuario;";
+
+		Statement statement = null;
+		Connection connection = DBConnection.getConnection();
+		try {
+			System.out.println("Connecting to a selected database...");
+			statement = connection.createStatement();
+			System.out.println("Connected database successfully...");
+
+			ResultSet rs = statement.executeQuery(sql);
+			while (rs.next()) {
+				l.add(new Usuario(rs.getInt("id"), rs.getString("name")));
+			}
+		} catch (SQLException se) {
+			// Handle errors for JDBC
+			se.printStackTrace();
+		} finally {
+			// finally block used to close resources
+			DBConnection.closeConnection(statement, connection);
+		} // end try
+
+		return l;
 	}
 
 	/**
@@ -104,17 +192,63 @@ public class UsuarioDao {
 	 *         id doesn't exist.
 	 */
 	public boolean replaceUser(Usuario usuario) {
-		if (!containsId(usuario.getId()))
-			return false;
+		String sql = "UPDATE usuario SET " + "name = '" + usuario.getNombre() + "' " + "WHERE id = '"
+				+ String.valueOf(usuario.getId()) + "';";
 
-		contentProvider.put(usuario.getId(), usuario);
+		Statement statement = null;
+		Connection connection = DBConnection.getConnection();
+		try {
+			System.out.println("Connecting to a selected database...");
+			statement = connection.createStatement();
+			System.out.println("Connected database successfully...");
+
+			statement.executeUpdate(sql);
+			System.out.println("Updated record from the table...");
+		} catch (SQLException se) {
+			// Handle errors for JDBC
+			se.printStackTrace();
+			return false;
+		} finally {
+			// finally block used to close resources
+			DBConnection.closeConnection(statement, connection);
+		} // end try
 		return true;
 	}
 
+	/**
+	 * Returns the user with the passed id. If the user doesn't exist a
+	 * NoSuchElementException is thrown.
+	 * 
+	 * @param id
+	 * @return
+	 * @throws NoSuchElementException
+	 */
 	public Usuario getUser(int id) throws NoSuchElementException {
-		if (!containsId(id))
-			throw new NoSuchElementException("User with id " + id + " not existing in List!");
-		return contentProvider.get(id);
+		String sql = "SELECT * FROM usuario WHERE id = '" + String.valueOf(id) + "';";
+
+		Statement statement = null;
+		Connection connection = DBConnection.getConnection();
+		try {
+			System.out.println("Connecting to a selected database...");
+			statement = connection.createStatement();
+			System.out.println("Connected database successfully...");
+
+			ResultSet rs = statement.executeQuery(sql);
+			if (!rs.next())
+				// No user returned from db
+				throw new NoSuchElementException("User with id " + id + " not existing!");
+			else
+				return new Usuario(rs.getInt("id"), rs.getString("name"));
+
+		} catch (SQLException se) {
+			// Handle errors for JDBC
+			se.printStackTrace();
+		} finally {
+			// finally block used to close resources
+			DBConnection.closeConnection(statement, connection);
+		} // end try
+
+		return null;
 	}
 
 }
--- UPMsocial/src/model/Usuario.java
+++ UPMsocial/src/model/Usuario.java
@@ -19,6 +19,12 @@ public class Usuario {
 		this.nombre = nombre;
 	}
 
+	public Usuario(int id, String nombre) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+	}
+
 	public int getId() {
 		return id;
 	}
